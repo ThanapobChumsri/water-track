6085d06013f2503af2a1cf52f24d354f13e22af3
ThanapobChumsri <watcha555@gmail.com>
2021-08-07 23:30:29 +0700

    create login ui

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,13 +1,58 @@
 <template>
-    <div></div>
+    <div>
+        <form>
+            <div>
+                <h3>Sign In</h3>
+
+                <div>
+                    <label>Email address </label>
+                    <input type="email" v-model="form.email">
+                </div>
+
+                <div>
+                    <label>Password </label>
+                    <input type="password" v-model="form.password">
+                </div>
+
+                <button type="submit" @click="addDataLogin">Sign In</button>
+
+                <p>
+                    Create new account
+                    <router-link to="/register">sign up?</router-link>
+                </p>
+
+            </div>
+        </form>
+    </div>
 </template>
 
 <script>
+import dataStore from '@/store/data'
 export default {
-
+    data() {
+            return {
+                form:{
+                    email:'',
+                    password:'',
+                    
+                }
+            }
+        },
+        methods:{
+            addDataLogin(){
+            let payload = {
+                email: this.form.email,
+                password: this.form.password,
+            }
+            dataStore.dispatch('addDataLogin', payload)
+            console.log(payload)
+            }
+        }
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+    div {
+        margin-bottom: 20px;
+    }
 </style>
