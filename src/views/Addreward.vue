8c94241c8d16461533f653eb2869f5afe700f2e6
j-heart <jakapat92@gmail.com>
2021-08-08 00:24:43 +0700

    make AddReward work

--- src/views/Addreward.vue
+++ src/views/Addreward.vue
@@ -6,6 +6,7 @@
         type="text"
         placeholder="Name"
         aria-label="default input example"
+        v-model="form.Name"
       />
     </div>
     <div class="mt-5">
@@ -14,6 +15,7 @@
         type="text"
         placeholder="Price "
         aria-label="default input example"
+        v-model="form.Price"
       />
     </div>
     <div class="mt-5">
@@ -22,6 +24,7 @@
         type="text"
         placeholder="Remaining"
         aria-label="default input example"
+        v-model="form.Remaining"
       />
     </div>
     <div class="mt-5">
@@ -30,18 +33,57 @@
         type="text"
         placeholder="Description"
         aria-label="default input example"
+        v-model="form.Description"
       />
     </div>
     <div class="mt-5">
       <p align="center">
-        <a class="btn btn-primary" href="#" role="button">Add</a>
+        <a @click="addReward" class="btn btn-primary" href="#" role="button">Add</a>
       </p>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+export default {
+  data() {
+    return {
+      form: {
+        Name: "",
+        Price: "",
+        Remaining: "",
+        Description: ""
+      }
+    }
+  },
+  methods: {
+    addReward() {
+      let payload = {
+        name: this.form.Name,
+        price: this.form.Price,
+        remaining: this.form.Remaining,
+        desc: this.form.Description
+      }
+
+      axios.post("http://localhost:1337/water-rewards", payload).then((res) => {
+        if (res.status ===200) {
+            return {
+              success: true,
+              data: res.data
+            }
+          } 
+        else {
+        console.error(res)
+        return {
+          success: true,
+          message: "Unknown status code: " + res.status
+        }
+      }
+      });
+    }
+  }
+};
 </script>
 
 <style>
