cf1491f7c1c91c50efd98733e255bd0dc35bb5a3
ThanapobChumsri <watcha555@gmail.com>
2021-08-08 19:55:31 +0700

    add vueSwal

--- src/main.js
+++ src/main.js
@@ -4,6 +4,12 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
 
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import VueSwal from 'vue-swal'
+
+Vue.use(VueAxios, axios)
+Vue.use(VueSwal)
 Vue.config.productionTip = false
 
 new Vue({
