06e789aee14441032daff072de9fda09a978a540
ThanapobChumsri <watcha555@gmail.com>
2021-08-10 13:49:22 +0700

    hide home for public

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -17,7 +17,7 @@
       </button>
       <div class="collapse navbar-collapse" id="navbarText">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
+          <li v-if="isAuthen()" class="nav-item">
             <a class="nav-link active" aria-current="page" href="/home">Home</a>
           </li>
           <li class="nav-item">
